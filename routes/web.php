46ce43b6fabe012bb42be05331ac63e7a4eef11a
raffaelecapaldo <raffaelecpl@gmail.com>
2023-05-19 14:01:14 +0200

    Added Hello World with a variable

--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,8 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    $data = [
+        'title' => 'Hello World',
+    ];
+    return view('home', $data);
 });
